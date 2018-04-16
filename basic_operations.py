9cd6e2cb62503440471e46629e7a13c2d73b48cf
sologuboved <sologuboved@gmail.com>
2018-04-17 00:34:17 +0300

    added utf json operation

--- basic_operations.py
+++ basic_operations.py
@@ -6,6 +6,16 @@ def load_json(json_file):
         return json.load(data)
 
 
+def load_utf_json(json_file):
+    with open(json_file, encoding='utf8') as data:
+        return json.load(data)
+
+
 def dump_json(entries, json_file):
     with open(json_file, 'w') as handler:
         json.dump(entries, handler)
+
+
+def dump_utf_json(entries, json_file):
+    with open(json_file, 'w', encoding='utf-8') as handler:
+        json.dump(entries, handler, ensure_ascii=False, sort_keys=True, indent=2)

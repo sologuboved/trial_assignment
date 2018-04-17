6911075b5126e7a5bb69c98ab5933c298402e652
sologuboved <sologuboved@gmail.com>
2018-04-17 16:48:15 +0300

    added ogrn finder

--- process_ngo_register71.py
+++ process_ngo_register71.py
@@ -1,3 +1,4 @@
+import re
 from basic_operations import load_utf_json, dump_utf_json
 
 RAW_JSON_FNAME = 'raw_ngo71.json'
@@ -15,10 +16,17 @@ OGRN = 'ogrn'
 def dump_orgs():
     orgs = load_utf_json(RAW_JSON_FNAME)
     for org in orgs:
-        for rubric in [TARGETS, ACTIVITIES, PROJECTS, SERVICES, CODES]:
+        for rubric in [TARGETS, ACTIVITIES, PROJECTS, SERVICES]:
             item = org[rubric]
             if item:
                 org[rubric] = str_to_list(item)
+        raw_codes = org[CODES]
+        if raw_codes:
+            codes = str_to_list(raw_codes)
+            org[CODES] = codes
+            org[OGRN] = find_ogrn(codes)
+        else:
+            org[OGRN] = None
 
     dump_utf_json(orgs, JSON_FNAME)
 
@@ -27,6 +35,21 @@ def str_to_list(string):
     return [item.strip() for item in string.split(DELIMITER) if item]
 
 
+def find_ogrn(codes):
+    for code in codes:
+        pattern = r'ОГРН[^0-9]*([0-9]*)'
+        try:
+            ogrn = re.findall(pattern, code)[0]
+        except IndexError:
+            continue
+        return ogrn
+
+
 if __name__ == '__main__':
-    # print(str_to_list(u"Развитие волонтерства и добровольчества*+*"))
+    c = [
+      "ИНН:7106042469",
+      "Код ОКПО (Росстат):57387361",
+      "Код ОКАТО:70401375000"
+    ]
+    # print(find_ogrn(c))
     dump_orgs()

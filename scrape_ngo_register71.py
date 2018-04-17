d4189d853df62e11c6c2c05b27a6f1e6e2bd2606
sologuboved <sologuboved@gmail.com>
2018-04-17 14:05:19 +0300

    minor refactoring

--- scrape_ngo_register71.py
+++ scrape_ngo_register71.py
@@ -29,27 +29,14 @@ def scrape_org(url):
     org = dict.fromkeys(RUBRICS)
     page_html = requests.get(url).content
     soup = BeautifulSoup(page_html, 'lxml')
-
-    org[ORGNAME] = soup.find_all('h1')[1].text.strip()
-
     raw_info = soup.find_all('article', {'class': "post clearfix"})[0]
-    # print(raw_info)
-
-    try:
-        website = raw_info.find('a', href=True).get('href')
-    except AttributeError:
-        pass
-    else:
-        if '.' in website:
-            org[WEBSITE] = website
-
     headers = raw_info.find_all('h3')
+
     for raw_header in headers:
         header = raw_header.text
-        print(header)
         if header not in HEADERS:
             continue
-        if header in (MISSION, OPF, HEAD, CONTACTS, MUNICIPALITY):
+        elif header in (MISSION, OPF, HEAD, CONTACTS, MUNICIPALITY):
             postfix = ' '
         else:
             postfix = '*+*'
@@ -69,11 +56,25 @@ def scrape_org(url):
                         new_info = new_info[1:]
                     info += new_info + postfix
                 org[HEADERS[header]] = info.strip()
+
+    org[ORGNAME] = scrape_orgname(soup)
+    org[WEBSITE] = scrape_website(raw_info)
+
     return org
 
 
-def scrape_mission():
-    pass
+def scrape_orgname(some_soup):
+    return some_soup.find_all('h1')[1].text.strip()
+
+
+def scrape_website(some_soup):
+    try:
+        website = some_soup.find('a', href=True).get('href')
+    except AttributeError:
+        return
+    else:
+        if '.' in website:
+            return website
 
 
 if __name__ == '__main__':
